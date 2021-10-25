5ab08cd8762cf2a1e3747e21c30710c2318b80ba
mb104 <mb104@mb104s-MacBook-Pro.local>
2021-10-25 11:46:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div>
+        <div >
+          <div className="overlay" />
+          <div className="container-fluid">
+            <div className="desc">
+              <h1>I am Home page</h1>
+              <h2>Associate Software Developer, Systango</h2>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+              <br/>
+              <br/>
+              <br/>
+            </div>
+          </div>
+        </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
